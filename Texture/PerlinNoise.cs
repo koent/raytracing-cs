025ab0e7f9ae04ae040cf7020003f9a6836a627b
koent <ktbbb5@gmail.com>
2023-01-22 20:43:41 +0100

    Scaled Perlin noise

--- Texture/PerlinNoise.cs
+++ Texture/PerlinNoise.cs
@@ -6,17 +6,20 @@ public class PerlinNoise : ITexture
 {
     private ITexture First, Second;
 
+    private double Scale;
+
     private Perlin Noise = new Perlin();
 
-    public PerlinNoise() : this(Color.Black, Color.White) { }
+    public PerlinNoise(double scale = 1.0) : this(Color.Black, Color.White, scale) { }
 
-    public PerlinNoise(Color first, Color second) : this(new SolidColor(first), new SolidColor(second)) { }
+    public PerlinNoise(Color first, Color second, double scale = 1.0) : this(new SolidColor(first), new SolidColor(second), scale) { }
 
-    public PerlinNoise(ITexture first, ITexture second)
+    public PerlinNoise(ITexture first, ITexture second, double scale = 1.0)
     {
         First = first;
         Second = second;
+        Scale = scale;
     }
 
-    public Color Value(double u, double v, Point3 point) => Color.Lerp(First.Value(u, v, point), Second.Value(u, v, point), Noise.Noise(point));
+    public Color Value(double u, double v, Point3 point) => Color.Lerp(First.Value(u, v, point), Second.Value(u, v, point), Noise.Noise(Scale * point));
 }
--- Vector/Point3.cs
+++ Vector/Point3.cs
@@ -22,4 +22,6 @@ public struct Point3
     public static Point3 operator +(Point3 left, Vec3 right) => new Point3(left.X + right.X, left.Y + right.Y, left.Z + right.Z);
 
     public static Point3 operator -(Point3 left, Vec3 right) => left + -right;
+
+    public static Point3 operator *(double scalar, Point3 point) => new Point3(scalar * point.X, scalar * point.Y, scalar * point.Z);
 }
